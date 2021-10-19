8b46ad1ea4c48ff4b07f79622ba5d00917cf28a1
jiqirui <1085436942@qq.com>
2021-10-20 01:06:19 +0800

    Ready for Heroku!

--- lib/wordguesser_game.rb
+++ lib/wordguesser_game.rb
@@ -4,9 +4,19 @@ class WordGuesserGame
   # to make the tests in spec/wordguesser_game_spec.rb pass.
 
   # Get a word from remote "random word" service
+  attr_accessor :word, :guesses, :wrong_guesses, :word_with_guesses, :count
 
   def initialize(word)
     @word = word
+    @guesses = ''
+    @wrong_guesses = ''
+    @word_with_guesses = ''
+    @count = 0
+    $i=0
+    while $i<@word.size do
+      $i+=1
+      @word_with_guesses=@word_with_guesses + '-'
+    end
   end
 
   # You can test it by installing irb via $ gem install irb
@@ -20,6 +30,45 @@ class WordGuesserGame
     Net::HTTP.new('randomword.saasbook.info').start { |http|
       return http.post(uri, "").body
     }
+  end
+
+  def guess(guess_letter)
+    raise ArgumentError if guess_letter == '' or guess_letter=~/[^a-zA-Z]/ or guess_letter==nil
+    guess_letter=guess_letter.downcase
+    if guesses.include? guess_letter or wrong_guesses.include? guess_letter
+      return false
+    elsif word.include? guess_letter
+      # Initialize a loop variable
+      len = 0
+      # do loop begins
+      loop do
+        if guess_letter == word[len]
+          word_with_guesses[len] = guess_letter
+        end 
+        len += 1
+        if (len == word.size)
+          break
+        end
+      end
+      @guesses = guess_letter
+      @count += 1
+      return true
+      
+    elsif !word.include? guess_letter 
+      @wrong_guesses = guess_letter
+      @count += 1
+      return true   
+    end
+  end
+
+  def check_win_or_lose()
+    if word_with_guesses == word
+      return :win
+    elsif @count >= 7
+      return :lose
+    else
+      return :play
+    end
   end
 
 end
